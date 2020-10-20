f788c59f3be8afe82f55fbb2bd3a98043b255f7e
lihongmin <abcd1234>
2020-10-20 16:37:28 +0800

    反汇编

---
+++ src/main/java/com/kevin/multithreading/geektime/other/sync/JavacJavap.java
@@ -0,0 +1,46 @@
+package com.kevin.multithreading.geektime.other.sync;
+
+public class JavacJavap {
+
+    /*Compiled from "Singleton.java"
+    public class com.kevin.multithreading.geektime.other.Singleton {
+        public static com.kevin.multithreading.geektime.other.Singleton getInstance();
+        Code:
+        0: getstatic     #1                  // Field singleton:Lcom/kevin/multithreading/geektime/other/Singleton;
+        3: ifnonnull     37
+        6: ldc           #2                  // class com/kevin/multithreading/geektime/other/Singleton
+        8: dup
+        9: astore_0
+        10: monitorenter
+        11: getstatic     #1                  // Field singleton:Lcom/kevin/multithreading/geektime/other/Singleton;
+        14: ifnonnull     27
+        17: new           #2                  // class com/kevin/multithreading/geektime/other/Singleton
+        20: dup
+        21: invokespecial #3                  // Method "<init>":()V
+        24: putstatic     #1                  // Field singleton:Lcom/kevin/multithreading/geektime/other/Singleton;
+        27: aload_0
+        28: monitorexit
+        29: goto          37
+        32: astore_1
+        33: aload_0
+        34: monitorexit
+        35: aload_1
+        36: athrow
+        37: getstatic     #1                  // Field singleton:Lcom/kevin/multithreading/geektime/other/Singleton;
+        40: areturn
+        Exception table:
+        from    to  target type
+        11    29    32   any
+        32    35    32   any
+
+        public static void main(java.lang.String[]);
+        Code:
+        0: invokestatic  #5                  // Method getInstance:()Lcom/kevin/multithreading/geektime/other/Singleton;
+        3: astore_1
+        4: getstatic     #6                  // Field java/lang/System.out:Ljava/io/PrintStream;
+        7: aload_1
+        8: invokevirtual #7                  // Method java/io/PrintStream.println:(Ljava/lang/Object;)V
+        11: return
+    }*/
+
+}
